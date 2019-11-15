e949906ee577f1359c198b5004a1b61d9fcef1bb
evelynjulia <evewicksteed@gmail.com>
2019-11-15 14:31:00 -0800

    got read_fwf working

--- project/scripts/sondes/get_soundings.py
+++ project/scripts/sondes/get_soundings.py
@@ -6,9 +6,14 @@
 
 
 import requests
+import pandas as pd
+import numpy as np
+import os
 
 
-
+# set paths:
+data_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/'
+script_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/scripts/'
 
 stn_no = '68816'
 region = 'africa'
@@ -24,14 +29,27 @@ start_url = 'http://weather.uwyo.edu/cgi-bin/sounding?region='
 
 url = start_url+region+'&TYPE=TEXT%3ALIST&YEAR='+yyyy+'&MONTH='+mm+'&FROM='+dd+hh+'&TO='+dd+hh+'&STNM='+stn_no
 
-
+#
 r = requests.get(url, allow_redirects=True)
-open('test_sounding.txt', 'wb').write(r.content)
+open(data_dir+'test_sounding.txt', 'wb').write(r.content)
 
-import pandas as pd
-df = pd.read_fwf('test_sounding.txt',skiprows = 10, header =)
 
-df2 = pd.read_fwf(url)
+#df = pd.read_fwf('test_sounding.txt',skiprows = 10, header =)
+col_names=['PRES','HGHT','TEMP','DWPT','RH','W','WDIR','WSPD','THETA','THTE','THETV']
+cols_to_use = np.arange(0,len(col_names))
+df = pd.read_fwf(url, skiprows = 10, names=col_names, skipfooter= 60,usecols=cols_to_use,widths=[7,7,7,7,7,7,7,7,7,7,7])
+
+
+
+# this is how to save to a csv file
+df2.to_csv(data_dir+'test_save_df_as_csv.csv')
+
+
+
+
+
+#np.genfromtxt(data_dir+'test_sounding.txt', skiprows=7, max_rows=1)
+
 
 
 '''
