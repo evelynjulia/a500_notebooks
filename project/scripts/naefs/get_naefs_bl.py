68f30da894d6bda7d229c06205a0115e1dbe12e7
evelynjulia <evewicksteed@gmail.com>
2019-11-18 14:47:05 -0800

    added tar dirs

--- project/scripts/naefs/get_naefs_bl.py
+++ project/scripts/naefs/get_naefs_bl.py
@@ -15,8 +15,6 @@ from datetime import datetime, timedelta
 # work flow: copy from source --> unzip --> process and save
 #source = '/Users/catherinemathews/Google Drive File Stream/Shared drives/Datamart/NAEFS/'
 src_dir = '/Volumes/GoogleDrive/Shared drives/Datamart/NAEFS/'
-#temp_folder = '/Volumes/Scratch/ewicksteed/data/naefs/'
-#final_folder
 data_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/naefs/'
 
 # team_drive = '/glade/scratch/ksha/DATA/NAEFS/'      # source dir
@@ -57,42 +55,47 @@ untar_cmd = 'tar -zxvf '+ filename + file_to_extract -C path
 ### get stuff ready for function
 
 # change directory first
-os.chdir(data_dir)
+os.chdir(data_dir) # this isn't working
 
 year='2016'
-month='05'
-day='09'
+month='06'
+day='10'
 hr='00'
 
 init_date= year+month+day+hr
-fcsthr = '012'
+fcsthr = '000'
+
+
+tar_src_dir = '/Volumes/GoogleDrive/Shared\ drives/Datamart/NAEFS/'
+tar_data_dir = '/Volumes/GoogleDrive/My\ Drive/Eve/courses/a500_notebooks_g/project/data/naefs/'
+
+
 # remove file path because we're in the right directory
-tar_cmd = 'tar -zxvf '+src_dir+init_date+'.tgz '+init_date+'/ncep_gec00.t00z.pgrb2f'+fcsthr #+' -C '+data_dir
+tar_cmd = 'tar -zxvf '+tar_src_dir+init_date+'.tgz '+init_date+'/ncep_gec00.t00z.pgrb2f'+fcsthr #+' -C '+data_dir
 
 print('Getting data for '+init_date+' for fcst hour '+fcsthr)
-subprocess.call(tar_cmd, shell=True)
+subprocess.call(tar_cmd, shell=True) # test without this first: , shell=True)
 
-'tar -zxvf /Volumes/GoogleDrive/Shared\ drives/Datamart/NAEFS/2016060100.tgz 2016060100/ncep_gec00.t00z.pgrb2f000 -C /Volumes/GoogleDrive/My\ Drive/Eve/courses/a500_notebooks_g/project/data/naefs/'
 'tar -zxvf /Volumes/GoogleDrive/Shared\ drives/Datamart/NAEFS/2016060100.tgz 2016060100/ncep_gec00.t00z.pgrb2f012 -C /Volumes/GoogleDrive/My\ Drive/Eve/courses/a500_notebooks_g/project/data/naefs/'
 
 
 
 # Maybe I must change directories first?
 
-# test
-temp_dir2 = "/temp_dir2/"
-local_space="/localspace/"
-kyle_cmd_tar = 'tar xvzf "'+temp_dir2+'" -C "'+local_space+'temp2/"'
 
 
 
 #### Create function:
 
+mod='ncep' # or cmc
+member='gec00'
 
-def get_naefs(year,month,day,hour,out_dir):
+
+def get_naefs(year,month,day,hour,fcst_hr,mod,member,out_dir):
 
     """
-    Downloads University of Wyoming soundings
+    Gets NAEFS data for specific model, init time, forecast hour and model and member from 
+    our google team drive and save to specified output directory
     
     parameters
     ==========
@@ -101,7 +104,13 @@ def get_naefs(year,month,day,hour,out_dir):
     month (MM): float or string
     day (DD): float or string
     hour (HH): float or string 
-        can be 00 or 12 in most cases  
+        initialization hour: we only download 00 
+    
+    mod: model (str)
+        ncep (US) or cmc (Canadian)
+    
+    member: model member (str)
+
 
 
     out_dir: Directory into which to save the data 
@@ -114,6 +123,9 @@ def get_naefs(year,month,day,hour,out_dir):
 
     """
 
+    # change dir because 'tar -C outdir' isn't working
+    os.chdir(out_dir)
+
     # convert to strings
     year = str(year)
     month = str(month)
@@ -124,8 +136,8 @@ def get_naefs(year,month,day,hour,out_dir):
     # set full date string:
     date_str = year+month+day+hour
 
-    # get url:
-    url = 'http://weather.uwyo.edu/cgi-bin/sounding?region='+region+'&TYPE=TEXT%3ALIST&YEAR='+year+'&MONTH='+month+'&FROM='+day+hour+'&TO='+day+hour+'&STNM='+stn
+    # naefs source on team drive:
+    src_dir = '/Volumes/GoogleDrive/Shared drives/Datamart/NAEFS/'
 
     # get / set column details
     col_names=['PRES','HGHT','TEMP','DWPT','RH','MIXR','WDIR','WSPD','THTA','THTE','THTV']
