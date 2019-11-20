7e550e48bc593323cf13e67a98fc92e9e668e8c0
Eve Wicksteed <ewicksteed@eoas.ubc.ca>
2019-11-20 15:50:51 -0800

    ran so now file list will be wrong, but works

--- project/scripts/naefs/run_subset.py
+++ project/scripts/naefs/run_subset.py
@@ -9,12 +9,17 @@
 import glob
 # import os
 from project.scripts.naefs.subset_naefs import subset_naefs
+#from subset_naefs import subset_naefs
+
 
 # get a list of all the files that I want to subset
-my_file_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/naefs/2016*/*'
+#my_file_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/naefs/2016*/*'
+# my_file_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/naefs/ncep_gec00.t00z.pgrb2f000'
 my_list_of_files = glob.glob(my_file_dir)
 
+
 # for cape town:
+# my_region = 'SA'
 my_region = 'SA'
 my_latN = '-20'
 my_latS = '-35'
@@ -23,6 +28,4 @@ my_lonE = '35'
 
 # call function:
 subset_naefs(my_latN, my_latS, my_lonW, my_lonE, my_region, my_list_of_files)
-
-
 
