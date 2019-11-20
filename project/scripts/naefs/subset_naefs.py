8e0c8df4b0965c65db5de8791f38bdb3965e5beb
Eve Wicksteed <ewicksteed@eoas.ubc.ca>
2019-11-20 15:47:38 -0800

    cleaned up a bit

--- project/scripts/naefs/subset_naefs.py
+++ project/scripts/naefs/subset_naefs.py
@@ -5,7 +5,6 @@
 # need to have wgrib2 installed on computer
 
 # imports
-import glob
 import os
 
 
@@ -18,7 +17,7 @@ def subset_naefs(latN, latS, lonW, lonE, region, list_of_files):
     parameters
     ==========
 
-    latN: north most latitude 
+    latN: north most latitude
         (int or str: will be converted to str)
     latS: south most latitude
         (int or str: will be converted to str)
@@ -46,17 +45,9 @@ def subset_naefs(latN, latS, lonW, lonE, region, list_of_files):
     lonW = str(lonW)
     lonE = str(lonE)
 
-
     for file in list_of_files:
-        #print(os.path.basename(file)+' in '+os.path.dirname(file))
         file_name = file.replace(' ', '\ ')
-        #subset_cmd = 'wgrib2 '+file+' -small_grib '+lonW+':'+lonE+' '+latS+':'+latN+' '+file+'_'+region
-        subset_cmd = 'wgrib2 '+new_file_name+' -small_grib '+lonW+':'+lonE+' '+latS+':'+latN+' '+new_file_name+'_'+region
-        print(subset_cmd)
-        #print(file.replace(' ', '\ '))
+        subset_cmd = 'wgrib2 '+file_name+' -small_grib '+lonW+':'+lonE+' '+latS+':'+latN+' '+file_name+'_'+region
         os.system(subset_cmd)
 
-
     return None
-
-
