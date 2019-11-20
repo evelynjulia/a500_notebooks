7fe919de4d351c646491c370685c18e2059c4d2f
evelynjulia <evewicksteed@gmail.com>
2019-11-20 15:13:04 -0800

    removed code except func

--- project/scripts/naefs/subset_naefs.py
+++ project/scripts/naefs/subset_naefs.py
@@ -58,30 +58,3 @@ def subset_naefs(latN, latS, lonW, lonE, region, list_of_files):
     return None
 
 
-# get a list of all the files that I want to subset
-my_file_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/data/naefs/2016*/*'
-my_list_of_files = glob.glob(my_file_dir)
-
-# for cape town:
-my_region = 'SA'
-my_latN = '-20'
-my_latS = '-35'
-my_lonW = '15'
-my_lonE = '35'
-
-# call function:
-subset_naefs(my_latN, my_latS, my_lonW, my_lonE, my_region, my_list_of_files)
-
-# for file in list_of_files:
-#     #print(os.path.basename(file)+' in '+os.path.dirname(file))
-#     subset_cmd = 'wgrib2 '+file+' -small_grib '+my_lonW+':'+my_lonE+' '+my_latS+':'+my_latN+' '+file+'_'+my_region
-#     print(subset_cmd)
-
-
-
-
-#subset_cmd = 'wgrib2 '+file+' -small_grib '+lonE+':'+lonW+' '+latS+':'+latN+' '+file+'_'+region
-
-
-
-
