b001102a786cb323c0bc3f2a0a084983501c6fd0
Eve Wicksteed <ewicksteed@eoas.ubc.ca>
2019-11-20 12:00:37 -0800

    flake 8 update

--- project/scripts/naefs/run_get_naefs.py
+++ project/scripts/naefs/run_get_naefs.py
@@ -14,30 +14,28 @@ data_dir = '/Volumes/GoogleDrive/My Drive/Eve/courses/a500_notebooks_g/project/d
 os.chdir(data_dir)
 
 # set params:
-mod='ncep' # or cmc
-member='gec00'
+mod = 'ncep'  # or cmc
+member = 'gec00'
 
 
 # year='2016'
 # month='06'
 # day='10'
-hr='00'
+hr = '00'
 
-#init_date= year+month+day+hr
-#fcsthr = '000'
+# init_date= year+month+day+hr
+# fcsthr = '000'
 
 for year in range(2016, 2017):
-    for month in range(6,9):
+    for month in range(6, 9):
         month = str(month).zfill(2)
-        for day in range(1,31):
+        for day in range(1, 32):
             day = str(day).zfill(2)
-            for fcsthr in ['000','006','012', '018']: 
+            for fcsthr in ['000', '006', '012', '018']:
                 filepath = str(year)+month+day+hr+'/'+mod+'_'+member+'.t00z.pgrb2f'+fcsthr
                 if path.exists(filepath):
                     print(filepath+' exists\n')
                 else:
                     print(filepath+' does not exist')
                     print("download for "+str(year)+month+day+hr+' and for forecast hour '+fcsthr+'\n')
-                    get_naefs(year,month,day,hr,fcsthr,mod,member,data_dir)            
-
-
+                    get_naefs(year, month, day, hr, fcsthr, mod, member, data_dir)
