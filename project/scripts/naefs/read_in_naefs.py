11f55ee0ac2b5a5e60fd98cc7f96782d96300b77
evelynjulia <evewicksteed@gmail.com>
2019-11-22 15:43:10 -0800

    working and cleaned

--- project/scripts/naefs/read_in_naefs.py
+++ project/scripts/naefs/read_in_naefs.py
@@ -7,41 +7,18 @@ from netCDF4 import Dataset
 import numpy as np
 import datetime as dt
 from a500.utils import ncdump
-#import matplotlib.pyplot as plt
 from netCDF4 import num2date, date2num
 import pandas as pd
-
 import os
-from pathlib import Path
+
 
 #data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/'
-#date_of_run = num2date(time,units=time_units)[0]
 
 list_of_files = glob.glob('/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/2016*/*.nc')
 
 
 ########################################################################
-######################## Eve's dataframe method ########################
-########################################################################
-#df = pd.DataFrame()
-
-for file in list_of_files[0:3]:
-    #print(file)
-    with Dataset(file,'r') as gec00:
-        hgt85 = gec00.variables['HGT_850mb'][0,...] # .byteswap().newbyteorder()
-        time=gec00.variables['time'][...]
-        time_units= gec00.variables['time'].units
-        flat_hgt85 = hgt85.flatten()# .byteswap().newbyteorder()
-        # get the date
-        date_of_run = num2date(time,units=time_units)[0]
-        date = dt.strftime(date_of_run,"%Y%m%d%H")
-        print(date)
-    # add data to a pd df with date as the column name
-    df[date] = flat_hgt85
-
-
-########################################################################
-######################## Chris' np array method ########################
+####################### Rodell's np array method #######################
 ########################################################################
 
 #print(path_str[0][-8:-3])
@@ -49,24 +26,34 @@ for file in list_of_files[0:3]:
 
 pathlist = sorted(Path(filein).glob(file+'*'))
 
-hgt85 = []
-for path in list_of_files[0:3]:
-    #print(os.path.basename(path)) # get the actual file name
-    eves_file = Dataset(path,'r') 
+hgt85, date = [], []
+for file in list_of_files:
+    print(os.path.basename(file)) # get the actual file name
+    eves_file = Dataset(file,'r') 
     #print(eves_file.variables.keys())
     
     #hgt85_i = np.array(eves_file.variables['HGT_850mb'][0,...])
-    # to get a flat array
-    hgt85_i = np.array(eves_file.variables['HGT_850mb'][0,...]).flatten() 
-    print(len(hgt85_i))
-    print(hgt85_i.shape)
-    #rh.append(rh_i)
+    hgt85_i = np.array(eves_file.variables['HGT_850mb'][0,...]).flatten() # to get a flat array
+
+    time=eves_file.variables['time'][...]
+
+    time_units= eves_file.variables['time'].units
+    # get the date
+    date_of_run = num2date(time,units=time_units)[0]
+    date_i = dt.datetime.strftime(date_of_run,"%Y%m%d%H")
+    print(date_i)
+
     hgt85.append(hgt85_i)
-    
+    date.append(date_i)
 
 
-hgt85_stack = np.stack(hgt85)
+all_hgt85 = np.stack(hgt85)
 
-#hgt85_stack.shape
+# the above corresponds to the following date order
+all_dates = np.stack(date)
 
-    
+########################################################################
+######################## to pandas dataframe ########################
+########################################################################
+
+naefs_df_hgt85 = pd.DataFrame(all_hgt85.T, columns = all_dates)
