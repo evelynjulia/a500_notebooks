94bf670805e3a384cf558d4cb14485ed323bd713
evelynjulia <evewicksteed@gmail.com>
2019-11-22 15:45:36 -0800

    file to plot soms

---
+++ project/scripts/naefs/naefs_soms.py
@@ -0,0 +1,12 @@
+# Eve Wicksteed
+#
+# 22 November 2019
+
+
+# now we want to unravel the array so we can feed it into SOMS
+
+# first unravel each day
+
+# then combine all into one 2D array with rows as data and columns as the lat and lon vars that have been flattened
+
+# then can feed into minisom SOM package 
