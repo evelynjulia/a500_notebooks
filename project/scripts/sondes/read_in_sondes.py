73c6b049c6099638d6736dd35ce8fdad2de1ea48
evelynjulia <evewicksteed@gmail.com>
2019-11-25 14:36:01 -0800

    loop works but not stacking bc dif size arr

--- project/scripts/sondes/read_in_sondes.py
+++ project/scripts/sondes/read_in_sondes.py
@@ -18,8 +18,66 @@ data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/'
 
 list_of_files = glob.glob('/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/*.csv')
 
+len_date = 10
+
 # get date
-for file in list_of_files:
-    print(file)
+date, theta_v = [], []
+for file in list_of_files[0:4]:
+    #print(file)
     #print(os.path.basename(file))
+    date_i = os.path.basename(file)[0:len_date]
+    print(date_i)
+    df = pd.read_csv(file)
+    if df.shape[0] > 0:
+        t_v_i = np.array(df['THTV'])
+        theta_v.append(t_v_i)
+        date.append(date_i)
+    else: 
+        print('Sounding dataframe is empty... skipping this date/time:', date_i)
+
+
+all_tv = np.stack(theta_v)
+
+# the above corresponds to the following date order
+all_dates = np.stack(date)
+
+sonde_df_tv = pd.DataFrame(all_tv.T, columns = all_dates)
     
+
+#pd.read_csv("/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/2016082712_sounding_68816.csv")
+
+
+#stn_no = os.path.basename(list_of_files[0])[20:25]
+#date = os.path.basename(list_of_files[0])[0:len_date]
+
+# do this for theta_v ('THTV')
+
+date = []
+for file in list_of_files:
+    
+    # print date
+    print(os.path.basename(file)) # get the actual file name
+    eves_file = Dataset(file,'r') 
+    #print(eves_file.variables.keys())
+    
+
+    #hgt85_i = np.array(eves_file.variables['HGT_850mb'][0,...])
+    hgt85_i = np.array(eves_file.variables['HGT_850mb'][0,...]).flatten() # to get a flat array
+
+    
+
+    
+    # get the date
+    #date_of_run = num2date(time,units=time_units)[0]
+    #date_i = dt.datetime.strftime(date_of_run,"%Y%m%d%H")
+    date_i = os.path.basename(file)[0:len_date]
+    print(date_i)
+
+    #hgt85.append(hgt85_i)
+    date.append(date_i)
+
+
+all_hgt85 = np.stack(hgt85)
+
+# the above corresponds to the following date order
+all_dates = np.stack(date)
