58a1ab48887261a1af65bb658574bba38bc52c69
evelynjulia <evewicksteed@gmail.com>
2019-11-26 11:13:22 -0800

    plots wind speed and theta v

--- project/scripts/sondes/read_in_sondes.py
+++ project/scripts/sondes/read_in_sondes.py
@@ -23,20 +23,26 @@ len_date = 10
 
 # read all into one dataframe and then groupby date
 fig, ax = plt.subplots()
+fig2, ax2 = plt.subplots()
 
 df_all = pd.DataFrame()
-for file in list_of_files:
+for file in list_of_files[0:20]:
     df = pd.read_csv(file, index_col= 'Unnamed: 0')
     date_i = os.path.basename(file)[0:len_date]
     if df.shape[0] > 0:
-        ax.plot(df['THTV'][0:20], df['HGHT'][0:20], '.-', label = date_i)
+        # PLOT
+        ax.plot(df['THTV'][df['HGHT'] < 2500], df['HGHT'][df['HGHT'] < 2500], '.-', label = date_i)
+        ax2.plot(df['WSPD'][df['HGHT'] < 2500], df['HGHT'][df['HGHT'] < 2500], '.-', label = date_i)
         df_all = df_all.append(df)
         print('Adding data for ', date_i)
         print(df_all.shape)
     else: 
         print(date_i,' sounding dataframe is empty... skipping this date/time.')
 
-
+ax.set_ylabel('Height')
+ax.set_xlabel('Theta V')
+ax2.set_ylabel('Height')
+ax2.set_xlabel('Wind speed')
 #plt.legend()
 plt.show()
 
