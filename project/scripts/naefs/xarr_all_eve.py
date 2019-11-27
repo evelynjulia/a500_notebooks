e59fe75440e6b2433cd322f5dc54dff0982bbfac
evelynjulia <evewicksteed@gmail.com>
2019-11-27 11:25:05 -0800

    works to sort files and save all to .nc file

--- project/scripts/naefs/xarr_all_eve.py
+++ project/scripts/naefs/xarr_all_eve.py
@@ -1,4 +1,7 @@
-
+# Eve Wicksteed
+# Edits to Phil's scripts
+# November 2019
+# Create xarray and copy to netcdf file
 import xarray
 from netCDF4 import Dataset
 from pathlib import Path
@@ -13,24 +16,45 @@ matplotlib.use("Agg")
 from a500.utils import ncdump
 import re
 
-#
+####### SET DIRECTORIES
+data_dir = "/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/"
+
+####### so here I want to loop through all the folders (probably in order)
+all_files = list(Path(data_dir).glob("2016*/*SA.nc"))
+pprint.pprint(all_files)
+
+
+####### NOW WANT TO SORT THE DATE TOO
+
 # the following regular expression captures one group
 # of exactly 3 characters, all numbers between 0-9
 # the filenames look like ncep_gec00.t00z.pgrb2f006_SA.nc
 #
 find_hour = re.compile(r".*grb2.*(\d{3,3}).*_SA.nc")
-#
-#
-#
-# data_dir = "/scratch/paustin/ewicksteed/mydata/2016060100"
-# all_files = list(Path(data_dir).glob("*.nc"))
-# pprint.pprint(all_files)
-data_dir = "/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/2016060100"
+# get just the date
+find_date = re.compile(r".*naefs/*(\d{10,10}).*_SA.nc")
 
-### so here I want to loop through all the folders (probably in order)
-all_files = list(Path(data_dir).glob("*SA.nc"))
-pprint.pprint(all_files)
 
+####### SOME SORTING FUNCTIONS
+
+def sort_full_date(the_file):
+    """
+    sort the files by converting the 10 digit initialization date to
+    an integer and the 3 digit time to
+    an integer and returning the combined initialization date and 
+    forecast hour time so I can sort by both to read in my files
+    """
+    date_match = find_date.match(str(the_file))
+    hour_match = find_hour.match(str(the_file))
+    return int(date_match.group(1)+hour_match.group(1))
+
+def sort_init_date(the_file):
+    """
+    sort the files by converting the 10 digit initialization date to
+    an integer and returning that number
+    """
+    the_match = find_date.match(str(the_file))
+    return int(the_match.group(1))
 
 def sort_hour(the_file):
     """
@@ -40,9 +64,10 @@ def sort_hour(the_file):
     the_match = find_hour.match(str(the_file))
     return int(the_match.group(1))
 
+
 ####### CREATE XARRAY
 if __name__ == "__main__":
-    all_files.sort(key=sort_hour)
+    all_files.sort(key=sort_full_date) # I've changed this to sort by date and then by forecast hour
     xarray_files = []
     for item in all_files:
         with Dataset(str(item)) as nc_file:
@@ -52,39 +77,52 @@ if __name__ == "__main__":
             xarray_files.append(ds)
     ds_big = xr.combine_nested(xarray_files, 'time')
 
+
+
+####### CONVERT TO NETCDF
+
+ds_big.to_netcdf(data_dir+'all_naefs.nc')
+
+####### READ IN XARRAY FROM NETCDF 
+ds = xr.open_dataset(data_dir+'all_naefs.nc')
+
 ####### GET AVERAGE
-    time_average = ds_big.mean('time') # this averages over all time so we 
-    # get the daily average in this case (for 4 files) over all the lons and lats
-    #
-    # time_average.data_vars
-    # time_average.coords
+    # time_average = ds_big.mean('time') # this averages over all time so we 
+    # # get the daily average in this case (for 4 files) over all the lons and lats
+    # #
+    # # time_average.data_vars
+    # # time_average.coords
 
 
 ####### GET VARIABLE NAMES
-    varnames = list(ds_big.variables.keys())
+    # varnames = list(ds_big.variables.keys())
     
 
 ####### CREATE SMALLER XARRAY FOR ONE VARIABLE§    
-    #
-    #
-    # create an xarray out of these files
-    #
-    vel_vals = [
-        'VVEL_200mb', 'VVEL_250mb', 'VVEL_500mb', 'VVEL_700mb', 'VVEL_925mb',
-        'VVEL_1000mb'
-    ]
-    vel_dict = {}
-    for key in vel_vals:
-        vel_dict[key] = ds_big.variables[key]
-    ds_small = xr.Dataset(vel_dict, ds_big.coords)
-    #
-    # select a slice for the first timestep
-    #
-    out = ds_small['VVEL_200mb'].isel(time=0, latitude=slice(2, 4), longitude=slice(2,4))
-    print(f"slice example {out}")
-    ds_small.attrs[
-        'history'] = f"written by {str(context.this_dir / 'eve_xarray.py')}"
-    ds_small.to_zarr(context.data_dir / "small_wvel", 'w')
+    # #
+    # #
+    # # create an xarray out of these files
+    # #
+    # vel_vals = [
+    #     'VVEL_200mb', 'VVEL_250mb', 'VVEL_500mb', 'VVEL_700mb', 'VVEL_925mb',
+    #     'VVEL_1000mb'
+    # ]
+    # vel_dict = {}
+    # for key in vel_vals:
+    #     vel_dict[key] = ds_big.variables[key]
+    # ds_small = xr.Dataset(vel_dict, ds_big.coords)
+    
+    
+####### WRITE TO FILE
+    # #
+    # #
+    # # select a slice for the first timestep
+    # #
+    # out = ds_small['VVEL_200mb'].isel(time=0, latitude=slice(2, 4), longitude=slice(2,4))
+    # print(f"slice example {out}")
+    # ds_small.attrs[
+    #     'history'] = f"written by {str(context.this_dir / 'eve_xarray.py')}"
+    # ds_small.to_zarr(context.data_dir / "small_wvel", 'w')
 
 
 
