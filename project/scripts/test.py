5b2852e0e5624be5f49714b934f5b954da568d54
evelynjulia <evewicksteed@gmail.com>
2019-11-29 16:10:51 -0800

    works to import function

---
+++ project/scripts/test.py
@@ -0,0 +1,14 @@
+
+
+from project.scripts.functions import get_full_date
+from project.scripts.functions import get_overlap_dates
+
+# import get_full_date(the_file)
+
+naefs_data_dir = "/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/"
+naefs_files = "2016*/*SA.nc"
+
+sonde_data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/'
+sonde_files = "*.csv"
+
+test_func = get_overlap_dates(naefs_dir= naefs_data_dir, naefs_files= naefs_files, sonde_dir = sonde_data_dir, sonde_files = sonde_files)
