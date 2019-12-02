12ef3ae672f625b2900a3fb1b909565070bcbb36
evelynjulia <evewicksteed@gmail.com>
2019-12-02 13:17:41 -0800

    pprint

--- project/scripts/test.py
+++ project/scripts/test.py
@@ -2,7 +2,7 @@
 
 from project.scripts.functions import get_full_date
 from project.scripts.functions import get_overlap_dates
-
+import pprint
 # import get_full_date(the_file)
 
 naefs_data_dir = "/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/"
@@ -12,3 +12,6 @@ sonde_data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes
 sonde_files = "*.csv"
 
 test_func = get_overlap_dates(naefs_dir= naefs_data_dir, naefs_files= naefs_files, sonde_dir = sonde_data_dir, sonde_files = sonde_files)
+
+
+pprint.pprint(sorted(test_func))
