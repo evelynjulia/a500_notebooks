8bfca437a8b69d2192c62c23f38e9c5077c0630d
evelynjulia <evewicksteed@gmail.com>
2019-12-11 16:27:05 -0800

    added more stuff

--- project/scripts/test.py
+++ project/scripts/test.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+# Eve Wicksteed
+#
+# 11 December 2019
+
+
+# test getting overlapping dates
 
 from project.scripts.functions import get_full_date
 from project.scripts.functions import get_overlap_dates
@@ -16,3 +24,20 @@ dates_to_use = get_overlap_dates(naefs_dir= naefs_data_dir, naefs_files= naefs_f
 
 pprint.pprint(sorted(dates_to_use))
 print(len(dates_to_use))
+
+
+# test function to get sondes and plot 
+
+import glob
+from project.scripts.sondes.function_to_get_sondes import get_sonde_stabilty
+
+data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/'
+fig_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/figures/'
+
+list_of_files = sorted(glob.glob('/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/*.csv'))
+
+top_pres = 850
+stability_limit = 0.005 # what the cutoff is K/mb
+
+get_sonde_stabilty(data_dir, fig_dir, list_of_files, top_pres, stability_limit)
+
