cdbdfc145c6f14c40217680f1bba06319f3aed14
evelynjulia <evewicksteed@gmail.com>
2020-01-09 17:55:42 +0200

    added TOD and stability to naefs data

--- project/scripts/test.py
+++ project/scripts/test.py
@@ -6,12 +6,26 @@
 # 11 December 2019
 
 
+
+
 # test getting overlapping dates
 
 from project.scripts.functions import get_full_date
 from project.scripts.functions import get_overlap_dates
 import pprint
 # import get_full_date(the_file)
+
+import glob
+from project.scripts.sondes.function_to_get_sondes import get_sonde_stabilty
+
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+
+import pandas as pd
+
 
 naefs_data_dir = "/Users/catherinemathews/UBC/a500_notebooks/project/data/naefs/"
 naefs_files = "2016*/*SA.nc"
@@ -28,8 +42,6 @@ print(len(dates_to_use))
 
 # test function to get sondes and plot 
 
-import glob
-from project.scripts.sondes.function_to_get_sondes import get_sonde_stabilty
 
 data_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/data/sondes/'
 fig_dir = '/Users/catherinemathews/UBC/a500_notebooks/project/figures/'
@@ -43,7 +55,7 @@ get_sonde_stabilty(data_dir, fig_dir, list_of_files, top_pres, stability_limit)
 
 
 ### Open a pickle file
-import pickle
+
 
 fn1 = 'df_of_all_sondes_interp.pkl'
 fn2 = 'df_of_all_sondes_orig.pkl'
@@ -57,7 +69,7 @@ orig_snds = pickle.load(pkl_file)
 pkl_file.close() 
 
 
-import pandas as pd
+
 
 snds_sm_dates = pd.DataFrame() # data frame of dates that over lap with naefs data
 
@@ -82,9 +94,7 @@ for i in range(len(interp_snds)):
 #grouped = snds_sm_dates.groupby('COMP_DATE') #???
 #grouped.first() # prints the first of each column
 
-import matplotlib.pyplot as plt
-import numpy as np
-import datetime as dt
+
 
 
 
@@ -112,7 +122,6 @@ plt.savefig(fig_dir+'bar_stab_classes_and_grad'+run_date+'run_stablim'+str(stabi
 
 # grouped by stability class
 gbs =  snds_sm_dates.groupby('STABILITY') 
-
 gbs.groups.keys()
 
 # sonde data to use where the pressure values are only for 850, 925 and 1000
@@ -142,6 +151,8 @@ plt.legend(stab)
 plt.title('Mean theta by stability class')
 plt.savefig(fig_dir+'mean_stab_class_sonde'+run_date+'run_stablim'+str(stability_limit)+'.png')
 
+
+
 fig, ax = plt.subplots(1,1, figsize=(15,9))
 ax.plot(lev_TOD_av.unstack().T.iloc[0], Pres)
 ax.plot(lev_TOD_av.unstack().T.iloc[1], Pres)
@@ -159,3 +170,69 @@ plt.savefig(fig_dir+'mean_TOD_sonde'+run_date+'run_stablim'+str(stability_limit)
 
 # then get naefs as well
 
+pkl_file = open(naefs_data_dir+'all_naefs_df.pkl', 'rb') 
+naefs_data = pickle.load(pkl_file) 
+pkl_file.close() 
+
+
+# make potential temp in df
+p0=1.e5
+Rd=287.  #J/kg/K
+cpd=1004.  #J/kg/K
+#CT_theta=CT_temp.variables.values*(p0/CT_press**(Rd/cpd))
+
+
+naefs_df_tu =  pd.DataFrame()
+naefs_df_tu['COMP_DATE'] = naefs_data['COMP_DATE'] 
+naefs_df_tu['TMP1000'] = naefs_data['TMP1000']
+naefs_df_tu['TMP925'] = naefs_data['TMP925']
+naefs_df_tu['TMP850'] = naefs_data['TMP850']
+
+naefs_df_tu['THTA1000'] = naefs_df_tu['TMP1000']*((p0/100000)**(Rd/cpd))
+naefs_df_tu['THTA925'] = naefs_df_tu['TMP925']*((p0/100000)**(Rd/cpd))
+naefs_df_tu['THTA850'] = naefs_df_tu['TMP850']*((p0/100000)**(Rd/cpd))
+
+naefs_df_tu['AV_GRAD'] = (naefs_df_tu['THTA1000'] - naefs_df_tu['THTA850'])/150
+
+
+naefs_day_means = naefs_df_tu.groupby(['COMP_DATE']).mean()
+all_neafs_means = naefs_day_means.mean()
+
+p1df = pd.DataFrame()
+p1df['COMP_DATE'] = naefs_df_tu['COMP_DATE']
+p1df['AV_GRAD'] = naefs_df_tu['AV_GRAD']
+p1df['PRES'] = 1000.0
+p1df['THTA'] = naefs_df_tu['THTA1000']
+
+p2df = pd.DataFrame()
+p2df['COMP_DATE'] = naefs_df_tu['COMP_DATE']
+p2df['AV_GRAD'] = naefs_df_tu['AV_GRAD']
+p2df['PRES'] = 925.0
+p2df['THTA'] = naefs_df_tu['THTA925']
+
+p3df = pd.DataFrame()
+p3df['COMP_DATE'] = naefs_df_tu['COMP_DATE']
+p3df['AV_GRAD'] = naefs_df_tu['AV_GRAD']
+p3df['PRES'] = 850.0
+p3df['THTA'] = naefs_df_tu['THTA850']
+
+new_naefs_df_tu = pd.concat([p1df,p2df,p3df])
+
+new_naefs_df_tu.sort_values(by=['COMP_DATE', 'PRES'])
+
+
+
+############ CALCULATE STABILITY FOR NAEFS DATA
+
+stability_conditions = [
+    new_naefs_df_tu['AV_GRAD'] >= stability_limit,
+    (new_naefs_df_tu['AV_GRAD'] < stability_limit) & (new_naefs_df_tu['AV_GRAD'] > -stability_limit),
+    new_naefs_df_tu['AV_GRAD'] <= -stability_limit ]
+
+stability_choices = ['unstable', 'neutral', 'stable']  #['unstable', 'neutral', 'stable']
+new_naefs_df_tu['STABILITY'] = np.select(stability_conditions, stability_choices)
+
+### GET TIME OF DAY COLUMN
+new_naefs_df_tu['TOD'] = new_naefs_df_tu['COMP_DATE'].str[-2:]
+
+new_naefs_df_tu
